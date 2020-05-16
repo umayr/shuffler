0449226930e6b8c73c857307c8da169bec94b5e7
umayr <umayr@users.noreply.github.com>
2020-05-16 01:40:24 +0000

    deploy: b20a0d8fd6d5d7f37ad5c111fe69418a2f220b73

---
+++ precache-manifest.be037728b93795c626b092e05506df20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b57dc067553b0c241c3e0eb86fcca6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3572488c9054bcf6cec",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "519e8d1281298d83bc5b",
+    "url": "/static/js/2.521d6de4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.521d6de4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3572488c9054bcf6cec",
+    "url": "/static/js/main.9774d30d.chunk.js"
+  },
+  {
+    "revision": "faefa98c980b61053fe3",
+    "url": "/static/js/runtime-main.013a250f.js"
+  }
+]);
