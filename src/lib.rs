cd4bd35dbe582853c9d0982f853628fae70a77ba
Umayr Shahid <umayr.shahid@gmail.com>
2021-06-28 00:11:34 -0400

    Add wasm layer
    (cherry picked from commit 2db52f8863b65327085eaa31730546640195a292)

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use rand::prelude::*;
+
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
 
@@ -52,7 +53,7 @@ pub fn shuffle<T: Clone>(list: &Vec<T>, seed: &Option<String>) -> Vec<T> {
 /// shuffler::pick(&src, 3, &seed);
 /// // [2, 1, 3]
 /// ```
-pub fn pick<T: Copy>(list: &Vec<T>, count: usize, seed: &Option<String>) -> Vec<T> {
+pub fn pick<T: Clone>(list: &Vec<T>, count: usize, seed: &Option<String>) -> Vec<T> {
     let src = shuffle(list, seed);
 
     let l = src.len();
@@ -71,7 +72,7 @@ pub fn pick<T: Copy>(list: &Vec<T>, count: usize, seed: &Option<String>) -> Vec<
             i = rng.gen_range(0..l);
         }
 
-        val.push(src[i]);
+        val.push(src[i].clone());
         ignore.push(i);
     }
 
@@ -92,9 +93,9 @@ pub fn pick<T: Copy>(list: &Vec<T>, count: usize, seed: &Option<String>) -> Vec<
 /// shuffler::group(&src, 10, &seed);
 /// // [1, 2, 3]
 /// ```
-pub fn group<T: Copy + PartialEq>(list: &Vec<T>, count: usize, seed: &Option<String>) -> Vec<Vec<T>> {
+pub fn group<T: Clone + PartialEq>(list: &Vec<T>, count: usize, seed: &Option<String>) -> Vec<Vec<T>> {
     if count == 0 {
-        return Vec::new()
+        return Vec::new();
     }
 
     let mut src = list.clone();
@@ -109,14 +110,10 @@ pub fn group<T: Copy + PartialEq>(list: &Vec<T>, count: usize, seed: &Option<Str
     let max = (l as f64 / count as f64).ceil() as i64;
 
     for _ in 0..max {
-        let v = pick(&src, count, seed)
-            .iter()
-            .map(|&v| {
-                src.remove(src.iter().position(|&x| v == x).unwrap());
+        let v = pick(&src, count, seed);
 
-                v
-            })
-            .collect::<Vec<T>>();
+        // remove the picked items from the `src`
+        src.retain(|x| !v.contains(x));
 
         val.push(v);
     }
@@ -139,6 +136,11 @@ mod tests {
 
         assert_ne!(src, res);
         assert_eq!(res.len(), src.len());
+
+        let src = vec!["1", "2", "3", "4", "5", "6"];
+        let (r0, r1) = (shuffle(&src, &seed), shuffle(&src, &seed));
+
+        assert_eq!(r0, r1);
     }
 
     #[test]
@@ -175,5 +177,76 @@ mod tests {
 
         let g3 = group(&src, 5, &seed);
         assert_eq!(g3, vec![src]);
+    }
+}
+
+
+pub mod wasm {
+    extern crate console_error_panic_hook;
+
+    use wasm_bindgen::prelude::*;
+    use js_sys::{JsString, Array};
+
+    use super::{shuffle, pick, group};
+
+    #[cfg(debug_assertions)]
+    use std::panic;
+
+    fn set_hook() {
+        #[cfg(debug_assertions)]
+            panic::set_hook(Box::new(console_error_panic_hook::hook));
+    }
+
+    fn into_vec(src: Array) -> Vec<JsValue> {
+        let mut list: Vec<JsValue> = Vec::new();
+        src.for_each(&mut |v, _, _| { list.push(v); });
+
+        list
+    }
+
+    fn into_array(src: Vec<JsValue>) -> Array {
+        let list = Array::new();
+        src.into_iter().for_each(|v| { list.push(&v); });
+
+        list
+    }
+
+
+    #[wasm_bindgen(js_name = shuffle)]
+    pub fn js_shuffle(list: Array, seed: JsString) -> Array {
+        set_hook();
+
+        let list = into_vec(list);
+        let seed = Some(String::from(seed));
+
+        into_array(shuffle(&list, &seed))
+    }
+
+    #[wasm_bindgen(js_name = pick)]
+    pub fn js_pick(list: Array, count: usize, seed: JsString) -> Array {
+        set_hook();
+
+        let list = into_vec(list);
+        let seed = Some(String::from(seed));
+
+        into_array(pick(&list, count, &seed))
+    }
+
+
+    #[wasm_bindgen(js_name = group)]
+    pub fn js_group(list: Array, count: usize, seed: JsString) -> Array {
+        set_hook();
+
+        let seed = Some(String::from(seed));
+        let list = into_vec(list);
+
+        let res = group(&list, count, &seed)
+            .into_iter()
+            .map(into_array)
+            .map(JsValue::from)
+            .collect();
+
+
+        into_array(res)
     }
 }
